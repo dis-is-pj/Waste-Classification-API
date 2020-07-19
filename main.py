9a0e319d340ef4f2f7052ede9de6e40bcf58fc64
Prashant Jha <prashantjha.jec@gmail.com>
2020-07-19 17:54:17 +0530

    Commented all functiions and performed them in class.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from io import BytesIO
 
 # Load the model
 model = load_model('keras_model.h5')
-
+'''
 def get_img(url):
     response = req.get(url)
     img = Image.open(BytesIO(response.content))
@@ -30,7 +30,7 @@ def img_preprocessing(url):
 def predict(url):
     img = img_preprocessing(url)
     return(np.argmax(model.predict(img)))
-
+'''
 app = Flask(__name__)
 api = Api(app)
 
@@ -38,7 +38,13 @@ api = Api(app)
 class GetPrediction(Resource):
     def get(self, filename):
         url = 'https://binary-cdk.herokuapp.com/static/uploads/' + filename
-        label = predict(url)
+        response = req.get(url)
+        image = Image.open(BytesIO(response.content))
+        image = ImageOps.fit(image, (224,224), Image.ANTIALIAS)
+        image = np.asarray(image)
+        image = (image.astype(np.float32) / 127.0) - 1
+        image = np.array([image])
+        label = np.argmax(model.predict(image))
         return int(label)
     
 api.add_resource(GetPrediction, '/<string:filename>')
