895e602419c78463866428b41474022b2627f0b8
nayakanjali99 <30684876+nayakanjali99@users.noreply.github.com>
2020-07-22 15:30:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,30 @@ import os
 #from flask_ngrok import run_with_ngrok
 from fastai.vision import load_learner, open_image
 from flask_restful import Resource, Api
+import pyrebase
+config={
+    "apiKey": "AIzaSyBumH3t0dgqUQfNRx0lhZdrp4UcA0s6r7o",
+    "authDomain": "sih-db-3b091.firebaseapp.com",
+    "databaseURL": "https://sih-db-3b091.firebaseio.com",
+    "projectId": "sih-db-3b091",
+    "storageBucket": "sih-db-3b091.appspot.com",
+    "messagingSenderId": "826516287205",
+    "appId": "1:826516287205:web:7421a9ecfb7ac8e7b02e0c"
+
+}
+
 
 learn = load_learner('')
 
 def pred(filename):
     url = 'https://binary-cdk.herokuapp.com/static/uploads/' + filename
     try:
-        r = requests.get(url, allow_redirects=True)
-        open('/tmp/image.jpg','wb').write(r.content)
+        firebase=pyrebase.initialize_app(config)
+        storage= firebase.storage()
+        pathoncloud='images/'+filename
+        pathlocal='/tmp/'+filename
+        storage.child(pathoncloud).download(pathlocal)
+
         img = open_image('/tmp/image.jpg')
         s = str(learn.predict(img)[0])
         return(s)
